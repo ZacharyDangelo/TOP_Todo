d04ea55eae0755cad3b96d39d4d7260bfc304956
Zach <zachdangelo100@gmail.com>
2022-04-15 18:35:17 -0400

    Added js for generating Todo cards and placing them in the DOM. Created a basic form for adding new todos.

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import Project from "./project";
 import "./style.css";
 
 
-var todo1 = new Todo("Testing123","","",1,"",false);
-var todo2 = new Todo("Testing456","","",1,"",false);
+var todo1 = new Todo("Testing123","","",1,"", false);
+var todo2 = new Todo("Testing456","","",2,"", true);
 var testTodos = [todo1,todo2];
 
 const displayController = ((document) =>{
     function displayTodos(todos){
         console.log(todos);
-        const todoContainer = document.querySelector(".content-container");
+        const todoContainer = document.querySelector(".todos");
         todos.forEach( (todo) => {
             const card = createTodoCard(todo);
             todoContainer.appendChild(card);
@@ -18,10 +18,39 @@ const displayController = ((document) =>{
     }
 
     function createTodoCard(todo){
-        console.log(todo);
-        const elem = document.createElement("p");
-        elem.textContent = todo.title;
-        return elem;
+        const card = document.createElement("div");
+        card.classList.add("todo-card");
+        card.classList.add(getTodoPriorityClass(todo));
+        if(todo.isComplete) card.classList.add("completed");
+
+        const checkBox = document.createElement("input");
+        checkBox.type = "checkbox";
+        checkBox.classList.add("todo-checkbox");
+        card.appendChild(checkBox);
+
+        const title = document.createElement("h4");
+        title.classList.add("todo-title");
+        title.textContent = todo.title;
+        card.appendChild(title);
+
+        const dateInput = document.createElement("input");
+        dateInput.classList.add("todo-date");
+        card.appendChild(dateInput);
+
+        const deleteButton = document.createElement("Button");
+        deleteButton.classList.add("todo-delete");
+        deleteButton.textContent = "X";
+        card.appendChild(deleteButton);
+        
+
+        return card;
+    }
+
+    function getTodoPriorityClass(todo){
+        if(todo.priority == 1) return "priority-one";
+        if(todo.priority == 2) return "priority-two";
+        if(todo.priority == 3) return "priority-three";
+        if(todo.priority == 4) return "priority-four";
     }
 
     return {displayTodos};
